0cb8080500a723524fa23964023e5de52a060af9
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-04-28 19:30:23 +0300

    server.js

--- MyProjectHTMLProf(JS)/server.js
+++ MyProjectHTMLProf(JS)/server.js
@@ -18,6 +18,83 @@ app.get('/goods', (req, res) => {
     });
 });
 
+app.get('/cart', (req, res) => {
+    fs.readFile('./db/cart.json', 'utf-8', (err, data) => {
+        if(err) {
+            console.error(err);
+            res.send('Произошла ошибка');
+        }
+
+        const cart = JSON.parse(data);
+
+        res.send({
+            total: cart.reduce((acc, item) => acc + item.quantity * item.price, 0),
+            items: cart.map((item) => ({ ...item, subtotal: item.price * item.quantity})),
+        });
+    });
+});
+
+app.post('/cart', (req, res) => {
+    fs.readFile('./db/cart.json', 'utf-8', (err, data) => {
+        if(err) {
+            console.error(err);
+            res.send('Произошла ошибка');
+        }
+
+        const cart = JSON.parse(data);
+        cart.push(req.body);
+        fs.writeFile('./db/cart.json', JSON.stringify(cart), () => {
+            res.send({ item: req.body, total: cart.reduce((acc, item) => acc + item.quantity * item.price, 0)});
+        });
+    });
+});
+
+app.patch('/cart/:id', (req, res) => {
+    fs.readFile('./db/cart.json', 'utf-8', (err, data) => {
+        if(err) {
+            console.error(err);
+            res.send('Произошла ошибка');
+        }
+
+        let cart = JSON.parse(data);
+
+        cart = cart.map((item) => {
+            if(+item.id === +req.params.id) {
+                return {...item, ...req.body};
+            }
+
+            return item;
+        })
+        fs.writeFile('./db/cart.json', JSON.stringify(cart), () => {
+            res.send({
+                item: cart.find((item) => +item.id === +req.params.id),
+                total: cart.reduce((acc, item) => acc + item.quantity * item.price, 0),
+            });
+        });
+    });
+});
+
+app.delete('/cart/:id', (req, res) => {
+    fs.readFile('./db/cart.json', 'utf-8', (err, data) => {
+        if(err) {
+            console.error(err);
+            res.send('Произошла ошибка');
+        }
+
+        let cart = JSON.parse(data);
+
+        const item = cart.find((item) => +item.id === +req.params.id);
+        cart = cart.filter((item) => +item.id !== +req.params.id);
+        
+        fs.writeFile('./db/cart.json', JSON.stringify(cart), () => {
+            res.send({ 
+                item,
+                total: cart.reduce((acc, item) => acc + item.quantity * item.price, 0)
+            });
+        });
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server has been started');
 })
